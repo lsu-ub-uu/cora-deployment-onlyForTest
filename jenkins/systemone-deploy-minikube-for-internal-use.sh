9549e968d79fdf06849565e3e775e40d463b804c
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-05-20 13:46:37 +0000

    add jenkins shell for minikube

--- jenkins/systemone-deploy-minikube-for-internal-use.sh
+++ jenkins/systemone-deploy-minikube-for-internal-use.sh
@@ -7,7 +7,7 @@ kubectl delete pv systemone-internal-archive-read-only-volume
 kubectl delete pv systemone-internal-converted-files-volume
 kubectl delete pv systemone-internal-converted-files-read-only-volume
 
-cd ../helm
+cd helm
 kubectl create namespace systemone-internal
 kubectl apply -f helm-systemone-minikube-persistent-volumes.yaml --namespace=systemone-internal
 helm install systemone-internal systemone --namespace systemone-internal --set deployFitNesse=true
