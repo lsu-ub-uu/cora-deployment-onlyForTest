bfc6a6ccca4e386afb536d0994ade3be3e2be509
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-05-21 08:29:26 +0000

    fix startup of servers

---
+++ jenkins/systemone-deploy-minikube-for-preview.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+helm uninstall systemone-preview -n systemone-preview
+
+kubectl delete pv systemone-preview-postgres-volume
+kubectl delete pv systemone-preview-archive-volume
+kubectl delete pv systemone-preview-archive-read-only-volume
+kubectl delete pv systemone-preview-converted-files-volume
+kubectl delete pv systemone-preview-converted-files-read-only-volume
+
+cd helm
+kubectl create namespace systemone-preview
+kubectl apply -f helm-systemone-minikube-persistent-volumes.yaml --namespace=systemone-preview
+helm install systemone-preview systemone --namespace systemone-preview --set deployFitNesse=true
